6d6ac84bb84cf502b30b5e26f738ad3b1604858e
steveck-chung <schung@mozilla.com>
2016-03-24 13:57:58 +0800

    update sensorweb

--- sensorweb-frontend/js/sensor-setup.js
+++ sensorweb-frontend/js/sensor-setup.js
@@ -1,3 +1,26 @@
 (function(){
+  var userId = $.url().param('userId');
+  var sensorName = $('#sensor-name');
+  var sensorLocation = $('#sensor-location');
+  var sensorDescription = $('#sensor-description');
+  var sensorProject = $('#sensor-project');
+
   $('select').material_select();
+
+  $('#setup-sensor').click(function() {
+    var name = sensorName.val();
+    $.post('sensors', {
+      userId: userId,
+      projectId: sensorProject.val(),
+      name: name,
+      description: sensorDescription.val(),
+      coords: sensorLocation.val()
+    })
+    .done(function(result) {
+      alert('Added the ' + name + ' sensor.');
+    })
+    .fail(function(err) {
+      console.error(err)
+    });
+  });
 })();
